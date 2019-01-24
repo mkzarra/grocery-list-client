da71d18614376f8b19eddd8e0753dc593bfdfaa6
mkzarra <mkzarra@gmail.com>
2019-01-24 16:24:31 -0500

    added sidedrawer and toolbar. mobile responsive. added 2hocs. added logout flow.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
+import Layout from './hoc/Layout/Layout';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
+          </Switch>
+        </Layout>
       </div>
     );
   }
